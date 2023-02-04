7a62c7199ff458f5cbfcdbc077b9b7da651847b4
dandidw <49770753+dandidw@users.noreply.github.com>
2023-02-04 15:18:07 +0700

    Update SosialisasiGempaBumi.py

--- SosialisasiGempaBumi.py
+++ SosialisasiGempaBumi.py
@@ -4,10 +4,9 @@ import folium
 import geopandas
 import leafmap.foliumap as leafmap
 import plotly_express as px
-from PIL import Image
 from streamlit_option_menu import option_menu
-icon = Image.open('icon.png')
-st.set_page_config(page_title="Sosialisasi Gempa", page_icon="icon")
+
+st.set_page_config(page_title="Sosialisasi Gempa")
 df1 = pd.read_csv("gempagempigisel.csv")
 df = df1.loc[:, ("tanggal", "waktu", "latitude", "longitude", "kedalaman", "magnitudo", "daerah")]
 m = leafmap.Map(location=[ -0.78927, 113.921327], tiles="Cartodb dark_matter", zoom_start=4)
